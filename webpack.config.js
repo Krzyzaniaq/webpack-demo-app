5a0b468067d78e78b3368d0bb1221f0061b107a4
Krzyzaniaq <kamil.krzyzaniak23@gmail.com>
2023-02-22 20:15:10 +0100

    Cache Busting and HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
         path: path.resolve(__dirname, "dist"),
+        filename: "[name][contenthash].js",
+        clean: true,
+        assetModuleFilename: '[name][ext]',
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
     module: {
         rules: [
             {
