7ff56503c179f75c8a2439ab521f0e250b2205b1
Krzyzaniaq <kamil.krzyzaniak23@gmail.com>
2023-02-23 21:51:44 +0100

    Handle images

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
+    output: {
+        path: require('path').resolve(__dirname,"dist"),
+        filename: 'bundle.js',
+        assetModuleFilename: 'images/[hash][ext][query]'
+    },
+
     plugins: [
         new HtmlWebpackPlugin({
         template: "./src/template.html"
@@ -17,6 +23,10 @@ module.exports = {
                     "css-loader",  //2. Turns css into commonjs
                     "sass-loader" //1. Turns sass into css
                 ]
+            },
+            {
+                test: /\.html$/,
+                use: "html-loader"
             }
         ]
     }
